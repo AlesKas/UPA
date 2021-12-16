c048190b19220d8b254e600a7de485a3a77a06fb
xjahod06 <jahoda.vojta@gmail.com>
2021-12-16 22:33:14 +0100

    bash scripts

---
+++ plot_query.sh
@@ -0,0 +1,5 @@
+cd src/
+python3 dotazA_plot.py
+python3 dotazB_plot.py
+python3 vlastniDotaz_plot.py
+cd ..
--- run_queries.sh
+++
@@ -1,5 +0,0 @@
-cd src/
-python3 dotazA.py
-python3 dotazB.py
-python3 dotazC.py
-python3 vlastniDotaz.py
